fd2d834a95bcae6af00d23e1c1d085fcdc326119
Nikka-Sher <npiterman@gmail.com>
2017-06-15 19:54:28 -0700

    Request permission for Push notifications

--- pushmynotifs/AppDelegate.swift
+++ pushmynotifs/AppDelegate.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
+import UserNotifications
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +20,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+     
+        
+        let types: UNAuthorizationOptions = [.alert, .badge, .sound]
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: types) { granted, error in
+            if error != nil {
+            print("error asking for permission")
+            
+            } else {
+                switch granted {
+                case true:
+                    NotificationCenter.default.addObserver(self, selector: #selector(self.tokenRefreshNotification(notification:)), name: NSNotification.Name.firInstanceIDTokenRefresh, object: nil)
+                    application.registerForRemoteNotifications()
+                    print("notifications authorized")
+                case false:
+                    print("access not granted")
+                }
+            }
+        }
+   
+        
+        
+        
+        FIRApp.configure()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.tokenRefreshNotification(notification:)), name: NSNotification.Name.firInstanceIDTokenRefresh, object: nil)
         return true
     }
 
@@ -27,6 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        FIRMessaging.messaging().disconnect()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -35,12 +67,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        connectToFCM()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func tokenRefreshNotification(notification: NSNotification)
+    {
+        let refreshedToken = FIRInstanceID.instanceID().token()
+        print("InstanceID token: \(String(describing: refreshedToken))")
+        
+        connectToFCM()
+    }
+    
 
+    func connectToFCM() {
+        FIRMessaging.messaging().connect { (error) in
+            if (error != nil) {
+                print("unable to connect \(String(describing: error))")
+                
+            } else {
+                print("connected to FCM")
+            }
+        
+    }
+    
 }
 
+}
